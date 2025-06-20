0018ae250942df89560846757d13acf2c0ad7e8f
realyyt <emmyisaac60@gmail.com>
2025-06-20 01:57:52 +0100

    dfgfd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
 });
